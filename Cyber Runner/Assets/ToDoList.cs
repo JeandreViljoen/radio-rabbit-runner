4e9b77cd26f565a89eea24fdf97f9fe6ab70be20
JeandreViljoen <51402377+JeandreViljoen@users.noreply.github.com>
2024-08-08 23:26:20 +0200

    Update ToDoList.cs

--- Cyber Runner/Assets/ToDoList.cs
+++ Cyber Runner/Assets/ToDoList.cs
@@ -2,16 +2,12 @@
 
 
 * Do not allow new weapons or perks to spawn once the slots have been filled in.
-* UI for weapons/perks
  * when running out of either weapon or perk drafts it breaks drafting because there is no safety check for wehn a random draft returns 0
  *
  * 
  * Powerups (Health, attack speed, shield, EXP)
- * Set camera and tv positions for start, safe and dead states
- * UI For cards
  * Main menu tutorial
  * UI for stats screen
- * HUD elements (incl weapon and perk slots)
  * Implement more weapons (+3)
  * Implement more perks (+5)
  * Fix Combo upgrades
@@ -24,7 +20,7 @@
  * XXX UNLOCKED POPUP! after drafting
  * Show how many drafts are remaining
  * Fix safezone breaking if no drafts possible and continue
- * Dont track stacks until game has started
+ * Dont track stats until game has started
  * 
  * 
  * Set up user testing survey
